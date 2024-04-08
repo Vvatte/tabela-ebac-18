a326966eab849ecc435ac64cf69519ea87a57213
Vvatte <vvatte@yahoo.com>
2024-04-08 14:30:18 -0300

    tarefa 19

--- src/br/com/vvatte/TabelaTeste.java
+++ src/br/com/vvatte/TabelaTeste.java
@@ -1,4 +1,9 @@
 package br.com.vvatte;
 @Tabela
 public class TabelaTeste {
+
+    public static void main(String[] args){
+        Class<Tabela> clazz = Tabela.class;
+        System.out.println(clazz);
+    }
 }
